aa26ed26965f26e157b20f8e3f26e69e621a0d03
arianspacerock <siropemptyspace@gmail.com>
2023-06-04 13:42:17 +0300

    ref: add new components & styles

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./Skills.module.css";
 import styleContainer from "../common/styles/Container.module.css";
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
@@ -8,7 +9,9 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title={"JavaScript"} description={"Основной язык программирования"}/>
+                    <Skill title={"React"} description={"Библиотека JavaScript"}/>
+                    <Skill title={"CSS"} description={"Стилизация"}/>
                 </div>
             </div>
         </div>
